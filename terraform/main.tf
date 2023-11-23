2acbf6179085f40cb81543583984cdefa56131b6
Oliver Salzburg <oliver.salzburg@gmail.com>
2023-11-23 20:54:45 +0100

    feat: Add IAM access

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,7 @@ data "aws_route53_zone" "kitten_science" {
   name = local.domain_name
 }
 
+# S3
 resource "aws_s3_bucket" "kitten_science_website" {
   bucket = "kitten-science-website"
 }
@@ -39,6 +40,7 @@ resource "aws_s3_bucket_website_configuration" "kitten_science_website" {
   }
 }
 
+# Certificate
 resource "aws_acm_certificate" "kitten_science_website" {
   domain_name       = local.domain_name
   key_algorithm     = "EC_prime256v1"
@@ -129,6 +131,8 @@ resource "aws_cloudfront_distribution" "kitten_science_website" {
 }
 
 resource "aws_route53_record" "kitten_science_website" {
+  for_each = toset([ "A", "AAAA" ])
+
   alias {
     evaluate_target_health = true
     name                   = aws_cloudfront_distribution.kitten_science_website.domain_name
@@ -136,6 +140,62 @@ resource "aws_route53_record" "kitten_science_website" {
   }
 
   name    = local.domain_name
-  type    = "A"
+  type    = each.key
   zone_id = data.aws_route53_zone.kitten_science.zone_id
+}
+
+# Maintainer Access
+data "aws_iam_policy_document" "kitten_science_maintainer_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::022327457572:user/oliver"
+      ]
+    }
+  }
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type = "Federated"
+      identifiers = [
+        "arn:aws:iam::022327457572:oidc-provider/token.actions.githubusercontent.com"
+      ]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        "repo:kitten-science/kitten-scientists:ref:refs/heads/main"
+      ]
+    }
+  }
+}
+resource "aws_iam_role" "kitten_science_maintainer" {
+  name               = "ks-website-maintainer"
+  assume_role_policy = data.aws_iam_policy_document.kitten_science_maintainer_assume_role.json
+}
+
+data "aws_iam_policy_document" "kitten_science_website_maintainer" {
+  statement {
+    effect  = "Allow"
+    actions = ["s3:*"]
+    resources = [
+      aws_s3_bucket.kitten_science_website.arn,
+      "${aws_s3_bucket.kitten_science_website.arn}/*"
+    ]
+  }
+}
+resource "aws_iam_policy" "kitten_science_website_maintainer" {
+  name        = "ks-website-maintainer"
+  description = "Allows changing the Kitten Science website."
+  policy      = data.aws_iam_policy_document.kitten_science_website_maintainer.json
+}
+resource "aws_iam_role_policy_attachment" "kitten_science_website_maintainer" {
+  role       = aws_iam_role.kitten_science_maintainer.name
+  policy_arn = aws_iam_policy.kitten_science_website_maintainer.arn
 }
