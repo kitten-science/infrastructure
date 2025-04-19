5cbc3e02fdf4061fa29a6e108d13398c1d1da8b3
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-04-19 20:52:07 +0200

    feat: Block public S3 access

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -74,10 +74,10 @@ resource "aws_s3_bucket_ownership_controls" "logs" {
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 
   provider = aws.global
 }
